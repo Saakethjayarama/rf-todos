937018a32b85571cafbeae69eaf30851ab2cbd24
saaketh <saakethaj@gmail.com>
2021-06-05 20:30:40 +0530

    todo done

--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -1,7 +1,53 @@
 import React from "react";
+import "./Todo.css";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import { makeStyles } from "@material-ui/core";
 
-function Todo() {
-  return <div className="Todo">This is todo</div>;
+const useStyles = makeStyles((theme) => ({
+  actionBtns: {
+    marginLeft: theme.spacing(2),
+  },
+  editBtn: {
+    "&:hover": {
+      color: "rgb(209, 209, 40)",
+    },
+  },
+  deleteBtn: {
+    "&:hover": {
+      color: "red",
+    },
+  },
+}));
+
+function Todo({ id, task, addedAt }) {
+  const classes = useStyles();
+
+  const handleDelete = () => {
+    console.log("Entered Delete");
+  };
+
+  const handleEdit = () => {
+    console.log("Entered Edit");
+  };
+
+  return (
+    <div className="Todo">
+      <div className="Todo__container">
+        <div className="Todo__container__task">This is todo</div>
+        <div className="Todo__container__action">
+          <EditIcon
+            onClick={handleEdit}
+            className={`${classes.actionBtns} ${classes.editBtn}`}
+          />
+          <DeleteForeverIcon
+            onClick={handleDelete}
+            className={`${classes.actionBtns} ${classes.deleteBtn}`}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Todo;
